8116ad58d2883777320dba78b5fcce6a71f6c904
Kefas Gisung <gisungkefas@gmail.com>
2023-01-07 17:22:09 +0100

    Initial commit

---
+++ src/main/java/com/kefas/diaryblog/DiaryBlogApplication.java
@@ -0,0 +1,13 @@
+package com.kefas.diaryblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kefas/diaryblog/DiaryBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefas.diaryblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
